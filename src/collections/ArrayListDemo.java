4533ae0c8f5e0fc340f74eccf098cb88c3dbb551
Mohammed Ishaq <mohammed2004ishaq@gmail.com>
2026-09-22 14:29:29 +0530

    Learning java started

---
+++ src/collections/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+    }
+}
